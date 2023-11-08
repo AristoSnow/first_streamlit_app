cbb31cfbc83b65628c18262110656056e6c15667
AristoSnow <150255175+AristoSnow@users.noreply.github.com>
2023-11-08 14:15:12 +0100

    Update streamlit_app.py
    Added some lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatemeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
+
